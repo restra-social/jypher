56390e47f39dd6c5d96a4ced90dd185e73ce11b1
Fahim Shariar <fahim.shoumik@gmail.com>
2017-08-22 14:28:12 +0600

    refractor test code into importable library

---
+++ jypher.go
@@ -0,0 +1,132 @@
+package jypher
+
+import (
+	"fmt"
+	"github.com/kite-social/jypher/generator"
+	"github.com/kite-social/jypher/models"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+// Jypher struct
+type Jypher struct {
+	Graph    map[string]models.Graph
+	GraphObj models.JSONInfo
+	ID       string
+	Tree     []string
+
+	Master       string
+	ObjIteration int
+}
+
+// GetJypher build Object , Init method mostly
+func (j *Jypher) GetJypher(jsonInfo *models.JSONInfo) map[string]models.Graph {
+
+	j.Graph = map[string]models.Graph{}
+	j.Master = strings.ToLower(jsonInfo.Master)
+	j.ID = jsonInfo.ID
+	j.ObjIteration = 0
+
+	j.generateGraph(j.Master, jsonInfo.DecodedJSON, jsonInfo.Rules)
+
+	return j.Graph
+}
+
+// BuildCypher : Builds Cypher Query based on Graph Object
+func (j *Jypher) BuildCypher() string {
+
+	generator := generator.CypherGenerator{}
+	cypher := generator.Generate(j.ID, j.Graph, j.Tree)
+	return cypher
+}
+
+func (j *Jypher) generateGraph(node string, unmarshal map[string]interface{}, rules map[string]interface{}) map[string]models.Graph {
+
+	// match node in the map if not exists then create
+	// if exists then skip.
+	// Added to avoid duplicate entry in the model
+
+	nodeName := regexp.MustCompile(`[A-za-z]+`).FindAllString(node, -1)[0]
+
+	if name, ok := rules[nodeName]; ok {
+		node = regexp.MustCompile(`[A-za-z]+`).ReplaceAllString(node, name.(string))
+	}
+
+	if _, ok := j.Graph[node]; !ok {
+
+		j.Graph[node] = models.Graph{}
+
+		var g models.Graph
+		g.Nodes.Lebel = node
+
+		g.Edges.Source = j.Master
+		g.Edges.Target = node
+
+		j.Tree = append(j.Tree, node)
+		//fmt.Println(parents)
+
+		if j.Master == node {
+			// only the master node has the id
+			g.Nodes.ID = j.ID
+		}
+
+		for k, v := range unmarshal {
+
+			var data map[string]interface{}
+
+			t := reflect.ValueOf(v)
+			switch t.Kind() {
+			case reflect.String:
+				pro := map[string]interface{}{
+					k: v,
+				}
+				// if there exists a field called reference then
+				// there should be existing node that it is referring to
+				// so add the reference id to node id
+				if len(pro) > 0 {
+					if ref, ok := pro["reference"]; ok {
+						g.Nodes.ID = ref.(string)
+					}
+				}
+				g.Nodes.Properties = append(g.Nodes.Properties, pro)
+
+				j.Graph[node] = g
+
+			case reflect.Map:
+				data, _ = t.Interface().(map[string]interface{})
+
+				j.Master = node
+				j.generateGraph(k, data, rules)
+				// loop should reset if we found any object
+				j.ObjIteration = 0
+
+			case reflect.Slice:
+				slice := reflect.ValueOf(t.Interface())
+				length := slice.Len()
+				for i := 0; i < length; i++ {
+					val := reflect.ValueOf(slice.Index(i).Interface())
+					switch val.Kind() {
+					case reflect.String:
+						// make a saperate node with array of string !! #todo
+
+					case reflect.Map:
+						data, _ = slice.Index(i).Interface().(map[string]interface{})
+						j.Master = node
+						j.generateGraph(fmt.Sprintf("%s%d", k, j.ObjIteration), data, rules)
+						j.ObjIteration++
+					}
+				}
+			}
+
+			j.Graph[node] = g
+		}
+
+	} else {
+		// If duplicate found skip the parent but process the child #todo
+
+		//generateGraph(id, master, unmarshal, graph)
+	}
+
+	return j.Graph
+}
--- models/graph.go
+++ models/graph.go
@@ -20,3 +20,13 @@ type Edges struct {
 	Relation   string                   `json:"relation,omitempty"`
 	Properties []map[string]interface{} `json:"properties,omitempty"`
 }
+
+// JSONInfo struct contains the unmarshal JSON and the set of rules
+// that might apply during Graph Model decoding
+// along with the resource name and id
+type JSONInfo struct {
+	DecodedJSON map[string]interface{}
+	Rules       map[string]interface{}
+	ID          string
+	Master      string
+}
